20f458fb547c94dc75a5e6e8e0ed01285bce750d
Matheus <mat.araujoribeiro@gmail.com>
2024-01-30 15:54:52 -0300

    Nullable username

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
 using MinhaApi.Data;
 using MinhaApi.Models;
@@ -7,24 +8,32 @@ namespace MinhaApi.Controllers;
 [ApiController]
 public class HomeController : ControllerBase
 {
-    [HttpGet]
-    [Route("/{name}")]
-    public string WelcomeMessage(string name)
-    {
-        return $"Olá, {name}!";
-    }
+
 
     [HttpGet]
-    [Route("/")]
+    [Route("greeting/{name}")]
+    public string WelcomeMessage(string name)
+        => $"Olá, {name}!";
+
+    [HttpGet]
+    [Route("/alltodos")]
     public List<TodoModel> GetTodos([FromServices] AppDbContext context)
+        => context.Todos.ToList();
+
+    [HttpGet]
+    [Route("/{id}")]
+    public TodoModel GetById(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id
+    )
     {
-        return context.Todos.ToList();
+        return context.Todos.FirstOrDefault(x => x.Id == id);
     }
 
     [HttpPost]
     [Route("/")]
     public TodoModel Post(
-        [FromServices] AppDbContext context, 
+        [FromServices] AppDbContext context,
         [FromBody] TodoModel model)
     {
         context.Todos.Add(model);
@@ -32,11 +41,39 @@ public class HomeController : ControllerBase
         return model;
     }
 
-    // [HttpPut]
-    // [Route("/{id}?")]
-    // public TodoModel Update([FromServices] AppDbContext context, [FromBody] TodoModel model, int id)
-    // {
-    //     var user = context.Todos.Where(x=>x.Id == id);
+    [HttpPut]
+    [Route("{id:int}")]
+    public TodoModel Put(
+        [FromBody] TodoModel model,
+        [FromServices] AppDbContext context,
+        [FromRoute] int id
+    )
+    {
+        var todoItem = context.Todos.FirstOrDefault(x => x.Id == id);
 
-    // }
+
+        todoItem.Title = model.Title ?? todoItem.Title;
+        todoItem.Username = model.Username ?? todoItem.Title;
+        todoItem.IsDone = model.IsDone;
+
+        context.Todos.Update(todoItem);
+        context.SaveChanges();
+
+        return todoItem;
+    }
+
+    [HttpDelete]
+    [Route("/{id:int}")]
+    public string Delete(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id
+    )
+    {
+        var todoItem = context.Todos.FirstOrDefault(x => x.Id == id);
+        
+        context.Todos.Remove(todoItem);
+        context.SaveChanges();
+
+        return "item deletado";
+    }
 }
--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -3,8 +3,8 @@ namespace MinhaApi.Models;
 public class TodoModel
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Username { get; set; }
+    public string? Title { get; set; }
+    public string? Username { get; set; }
     public bool IsDone { get; set; }
     public DateTime CreatedAt { get; set; }
 }
